a5af34680fb912beb4d5fdf52773c5ee486ac845
nithishkumar-selvakumar <46522768+nithishkumar-selvakumar@users.noreply.github.com>
2019-07-31 20:29:14 +0530

    Create GUVI7.java

---
+++ GUVI7.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner ss=new Scanner(System.in);
+		int a=ss.nextInt();
+		for(int i=0;i<a;i++)
+		{
+			System.out.println("hello");
+		}
+	}
+}
