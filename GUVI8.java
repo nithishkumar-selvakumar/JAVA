8bad07e4e40d07cb169dd0fc9feb5c86d89e658e
nithishkumar-selvakumar <46522768+nithishkumar-selvakumar@users.noreply.github.com>
2019-07-31 20:48:23 +0530

    Create GUVI8.java

---
+++ GUVI8.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner aa=new Scanner(System.in);
+		int n=aa.nextInt();
+		int s=(n*(n+1))/2;
+		System.out.println("natural num is"+s);
+	}
+}
